45055b1cb36dd5d7455cb489a3ad3cb7cb4ac5ff
oscarvillena <oscarvillena12@gmail.com>
2023-05-10 19:40:55 +0200

    Cambios

---
+++ paginasweb/paginaprincipal.php
@@ -0,0 +1,35 @@
+<head>
+  <!--
+   -Criar um site de "vendas" basico
+   -com pagina principal, produtos, cadastro e contatos.
+  -->
+  <script src="https://code.jquery.com/jquery-1.10.1.min.js"></script>
+  <link rel="stylesheet" src="../principal.css">
+</head>
+<body>
+  <!-- -->
+  <header class="menu">
+    <h1>Rodrigo Anjos</h1>
+    <ul>
+      <li><a class="link-menu" href="#">Home</a></li>
+      <li><a class="link-menu" href="#">Produtos</a></li>
+      <li><a class="link-menu" href="#">Cadastro</a></li>
+      <li><a class="link-menu" href="#">Contato</a></li>      
+    </ul>
+  </header>
+  <section class="content">
+    <aside class="category">
+      <h3>Categorias</h3>
+      <ul class="list-category">
+      </ul>
+    </aside>
+    <div class="destaque-prod">
+      <!-- Produtos -->
+      <h2>Produtos recomendados</h2>
+      
+    </div>    
+  </section>
+  <footer class="footer">
+    
+  </footer>
+</body>
