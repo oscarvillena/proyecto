158183e574a4ba869f2a912893bd50c6bee2dc73
oscarvillena <oscarvillena12@gmail.com>
2023-05-10 20:45:45 +0200

    CSS PRINCIPAL ACABADO

--- paginasweb/paginaprincipal.php
+++ paginasweb/paginaprincipal.php
@@ -4,29 +4,24 @@
    -com pagina principal, produtos, cadastro e contatos.
   -->
   <script src="https://code.jquery.com/jquery-1.10.1.min.js"></script>
-  <link rel="stylesheet" src="../principal.css">
+  <link rel="stylesheet" href="../css/principal.css">
 </head>
 <body>
   <!-- -->
   <header class="menu">
-    <h1>Rodrigo Anjos</h1>
     <ul>
-      <li><a class="link-menu" href="#">Home</a></li>
-      <li><a class="link-menu" href="#">Produtos</a></li>
-      <li><a class="link-menu" href="#">Cadastro</a></li>
-      <li><a class="link-menu" href="#">Contato</a></li>      
+      <li><a class="link-menu" href="#">Iniciar Sesion</a></li>      
     </ul>
   </header>
   <section class="content">
     <aside class="category">
-      <h3>Categorias</h3>
-      <ul class="list-category">
-      </ul>
     </aside>
     <div class="destaque-prod">
       <!-- Produtos -->
-      <h2>Produtos recomendados</h2>
-      
+      <h2>BIENVENIDO</h2>
+      <img class="logo" src="../imagenes/logo.png">
+      <h2><a href="#">RESERVAR CITA</a></h2>
+      <h2><a href="#">QUIENES SOMOS</a></h2>
     </div>    
   </section>
   <footer class="footer">
