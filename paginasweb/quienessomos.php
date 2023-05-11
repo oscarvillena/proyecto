065b05306f5cccae1d597dd034323fbf9f6101d6
oscarvillena <oscarvillena12@gmail.com>
2023-05-11 16:21:45 +0200

    Commit Quienes Somos

---
+++ paginasweb/quienessomos.php
@@ -0,0 +1,29 @@
+<head>
+  <!--
+   -Criar um site de "vendas" basico
+   -com pagina principal, produtos, cadastro e contatos.
+  -->
+  <script src="https://code.jquery.com/jquery-1.10.1.min.js"></script>
+  <link rel="stylesheet" href="../css/quienessomos.css">
+</head>
+<body>
+  <!-- -->
+  <header class="menu">
+    <ul>
+      <li><a class="link-menu" href="#">Inicio</a></li>      
+    </ul>
+  </header>
+  <section class="content">
+    <aside class="category">
+    </aside>
+    <div class="destaque-prod">
+      <!-- Produtos -->
+      <h2>UBICACION</h2>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d53.37428109708708!2d-0.8594870237261791!3d41.67735168202553!2m3!1f191.5119590236925!2f45.03620577295978!3f0!3m2!1i1024!2i768!4f35!5e1!3m2!1ses!2ses!4v1683812380576!5m2!1ses!2ses" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <h2>Somos un taller ubicado en el poligono cogullada somos el mejor en nuestro sector si quieres qué hagamos a tu vehiculo ven.</h2>
+    </div>    
+  </section>
+  <footer class="footer">
+    
+  </footer>
+</body>
