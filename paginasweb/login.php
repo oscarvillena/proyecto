38d4dc2c1f3cba9ee3452c3fa5cba99754c6fffa
oscarvillena <oscarvillena12@gmail.com>
2023-05-14 19:02:41 +0200

    HTMLS Y CSS COMPLETOS

---
+++ paginasweb/login.php
@@ -0,0 +1,28 @@
+<head>
+  <!--
+   -Criar um site de "vendas" basico
+   -com pagina principal, produtos, cadastro e contatos.
+  -->
+  <script src="https://code.jquery.com/jquery-1.10.1.min.js"></script>
+  <link rel="stylesheet" href="../css/login.css">
+  
+</head>
+<body>
+  <!-- -->
+  <header class="menu">
+    <ul>
+      <li><a class="link-menu" href="#">Inicio</a></li>      
+    </ul>
+  </header>
+  <div class="wrapper">
+    <form class="form-signin">       
+      <h2 class="form-signin-heading">Iniciar</h2>
+      <input type="text" class="form-control" name="username" placeholder="Empleado" required="" autofocus="" />
+      <input type="password" class="form-control" name="password" placeholder="Cod_Em" required=""/>      
+      <p><button class="btn btn-lg btn-primary btn-block" type="submit">Login</button></p> 
+    </form>
+  </div>
+  <footer class="footer">
+    
+  </footer>
+</body>
---
+++ paginasweb/servicios.php
@@ -0,0 +1,38 @@
+<head>
+  <!--
+   -Criar um site de "vendas" basico
+   -com pagina principal, produtos, cadastro e contatos.
+  -->
+  <script src="https://code.jquery.com/jquery-1.10.1.min.js"></script>
+  <link rel="stylesheet" href="../css/cliente.css">
+</head>
+<body>
+  <!-- -->
+  <header class="menuheader">
+    <ul>
+      <li><a class="link-menu" href="#">Iniciar Sesion</a></li>      
+    </ul>
+  </header>
+  <div class="lateral">
+<ul class="menuVert">
+<li><a href="empleados.php">Empleados</a></li>
+<li><a href="facturas.php">Facturas</a></li>
+<li><a href="clientes.php">Clientes</a></li>
+<li><a href="vehiculos.php">Vehiculos</a></li>
+</ul>
+</div>
+  <section class="content">
+    <aside class="category">
+    </aside>
+    <div class="rectangulo">
+    <a href="#">Añadir Servicio</a>
+</div>
+    <div class="destaque-prod">
+      <!-- Produtos -->
+      <h2>TABLA Servicio</h2>
+    </div>    
+  </section>
+  <footer class="footer">
+    
+  </footer>
+</body>
