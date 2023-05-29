2eb41a9a1262f6d0041b309f551d87ba8a6ec1fa
oscarvillena <oscarvillena12@gmail.com>
2023-05-29 22:08:12 +0200

    Vehiculo Completo

--- paginasweb/facturas.php
+++ paginasweb/facturas.php
@@ -16,75 +16,208 @@
 </head>
 <body>
   <header class="menuheader">
-  <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" style="display: inline;">
-  <input type="hidden" name="logout">
-  <button type="submit" class="button-primary">Cerrar Sesión</button>
-</form>    
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" style="display: inline;">
+      <input type="hidden" name="logout">
+      <button type="submit" class="button-primary">Cerrar Sesión</button>
+    </form>
   </header>
+
   <div class="lateral">
     <div class="btn-group-vertical">
-    <a href="clientes.php" class="btn btn-drak btn-block" role="button">Clientes</a>
-  <a href="empleados.php" class="btn btn-drak btn-block" role="button">Empleados</a>
-  <a href="servicios.php" class="btn btn-drak btn-block" role="button">Servicios</a>
-  <a href="vehiculos.php" class="btn btn-drak btn-block" role="button">Vehiculos</a>
-</div>
-</div>
+      <a href="empleados.php" class="btn btn-drak btn-block" role="button">Empleados</a>
+      <a href="clientes.php" class="btn btn-drak btn-block" role="button">Clientes</a>
+      <a href="servicios.php" class="btn btn-drak btn-block" role="button">Servicios</a>
+      <a href="vehiculos.php" class="btn btn-drak btn-block" role="button">Vehiculos</a>
+    </div>
+  </div>
+
   <section class="content">
     <aside class="category">
     </aside>
     <div class="rectangulo">
-    <a href="#" class="button-secondary">Añadir Factura</a>
-</div>
+      <a href="#" class="button-secondary" id="btnAddFacturas">Añadir Facturas</a>
+    </div>
     <div class="destaque-prod">
-    <?php 
-$username = "root"; 
-$password = "ASf1*7JOSEM"; 
-$database = "taller_augusta"; 
-$mysqli = new mysqli("localhost", $username, $password, $database);
-$query = "SELECT * FROM facturas";
+      <?php 
+      $username = "root"; 
+      $password = "ASf1*7JOSEM"; 
+      $database = "taller_augusta"; 
+      $mysqli = new mysqli("localhost", $username, $password, $database);
+      $query = "SELECT * FROM facturas";
 
+      echo '<table border="1" cellspacing="1" cellpadding="1" style="max-width: 500px;"> 
+            <tr> 
+                <td> <font face="Arial">IdFactura</font> </td> 
+                <td> <font face="Arial">Fecha</font> </td> 
+                <td> <font face="Arial">Estado</font> </td> 
+                <td> <font face="Arial">Cod_Cl</font> </td> 
+                <td> <font face="Arial">IdServicio</font> </td> 
+            </tr>';
 
-echo '<table border="1" cellspacing="1" cellpadding="1"> 
-      <tr> 
-          <td> <font face="Arial">IdFactura</font> </td> 
-          <td> <font face="Arial">Fecha</font> </td> 
-          <td> <font face="Arial">Estado</font> </td> 
-          <td> <font face="Arial">Cod_Cl</font> </td> 
-          <td> <font face="Arial">IdServicio</font> </td> 
-      </tr>';
+      if ($result = $mysqli->query($query)) {
+          while ($row = $result->fetch_assoc()) {
+              $field1name = $row["IdFactura"];
+              $field2name = $row["Fecha"];
+              $field3name = $row["Estado"];
+              $field4name = $row["Cod_Cl"];
+              $field5name = $row["IdServicio"]; 
 
-if ($result = $mysqli->query($query)) {
-    while ($row = $result->fetch_assoc()) {
-        $field1name = $row["IdFactura"];
-        $field2name = $row["Fecha"];
-        $field3name = $row["Estado"];
-        $field4name = $row["Cod_Cl"];
-        $field5name = $row["IdServicio"]; 
+              echo '<tr> 
+                        <td>'.$field1name.'</td> 
+                        <td>'.$field2name.'</td> 
+                        <td>'.$field3name.'</td> 
+                        <td>'.$field4name.'</td> 
+                        <td>'.$field5name.'</td> 
+                    </tr>';
+          }
+          $result->free();
+      } 
+      ?>
 
-        echo '<tr> 
-                  <td>'.$field1name.'</td> 
-                  <td>'.$field2name.'</td> 
-                  <td>'.$field3name.'</td> 
-                  <td>'.$field4name.'</td> 
-                  <td>'.$field5name.'</td> 
-              </tr>';
-    }
-    $result->free();
-} 
-?>
-<?php
-session_start();
+      <?php
+      session_start();
 
-if (isset($_POST['logout'])) {
-  session_unset();
-  header("Location: paginaprincipal.php");
-  exit();
-}
-?>
-    </div>    
+      if (isset($_POST['logout'])) {
+        session_unset();
+        header("Location: paginaprincipal.php");
+        exit();
+      }
+
+      if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["idFactura"]) && isset($_POST["fecha"]) && isset($_POST["estado"]) && isset($_POST["cod_Cl"]) && isset($_POST["idServicio"])) {
+        $idFactura = $_POST["idFactura"];
+        $fecha = $_POST["fecha"];
+        $estado = $_POST["estado"];
+        $cod_Cl = $_POST["cod_Cl"];
+        $idServicio = $_POST["idServicio"];
+
+        $username = "root"; 
+        $password = "ASf1*7JOSEM"; 
+        $database = "taller_augusta"; 
+        $mysqli = new mysqli("localhost", $username, $password, $database);
+
+        
+        $checkQuery = "SELECT IdFactura FROM facturas WHERE IdFactura = '$idFactura'";
+        $checkResult = $mysqli->query($checkQuery);
+        if ($checkResult->num_rows > 0) {
+          echo "El código de factura ya existe. Por favor, elija otro código.";
+        } else {
+          $query = "INSERT INTO facturas (IdFactura, Fecha, Estado, Cod_Cl, IdServicio) VALUES ('$idFactura', '$fecha', '$estado', '$cod_Cl', '$idServicio')";
+
+          if ($mysqli->query($query)) {
+            echo "La nueva factura se ha añadido correctamente a la base de datos.";
+          } else {
+            echo "Error al añadir la nueva factura: " . $mysqli->error;
+          }
+        }
+
+        $mysqli->close();
+
+        
+        header("Location: ".$_SERVER['PHP_SELF']);
+      }
+
+      session_start();
+
+      if (isset($_POST['logout'])) {
+        session_unset();
+        header("Location: paginaprincipal.php");
+        exit();
+      }
+
+      if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["codFa_eliminar"])) {
+        $codFa_eliminar = $_POST["codFa_eliminar"];
+
+        $username = "root"; 
+        $password = "ASf1*7JOSEM"; 
+        $database = "taller_augusta"; 
+        $mysqli = new mysqli("localhost", $username, $password, $database);
+
+        
+        $checkQuery = "SELECT IdFactura FROM facturas WHERE IdFactura = '$codFa_eliminar'";
+        $checkResult = $mysqli->query($checkQuery);
+        if ($checkResult->num_rows > 0) {
+          $deleteQuery = "DELETE FROM facturas WHERE IdFactura = '$codFa_eliminar'";
+          if ($mysqli->query($deleteQuery)) {
+            echo "La nueva factura se ha eliminado correctamente de la base de datos.";
+          } else {
+            echo "Error al eliminar la factura: " . $mysqli->error;
+          }
+        } else {
+          echo "El código de la factura no existe en la base de datos. Por favor, ingrese un código válido.";
+        }
+
+        $mysqli->close();
+
+        
+        header("Location: ".$_SERVER['PHP_SELF']);
+      }
+      ?>
+      <hr>
+      <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <input type="text" name="codFa_eliminar" placeholder="Código de la Factura a Eliminar" required>
+        <button type="submit" class="button-primary">Eliminar Factura</button>
+      </form>
+    </div>
   </section>
+
   <footer class="footer">
-    
-  </footer>
+  </footer>    
+
+  <script>
+    document.getElementById("btnAddFacturas").addEventListener("click", function(event) {
+      event.preventDefault(); 
+
+     
+      var form = document.createElement("form");
+      form.method = "post";
+      form.action = "<?php echo $_SERVER['PHP_SELF']; ?>";
+
+      var inputIdFactura = document.createElement("input");
+      inputIdFactura.type = "text";
+      inputIdFactura.name = "idFactura";
+      inputIdFactura.placeholder = "Código de factura";
+      inputIdFactura.required = true;
+
+      var inputFecha = document.createElement("input");
+      inputFecha.type = "text";
+      inputFecha.name = "fecha";
+      inputFecha.placeholder = "Fecha";
+      inputFecha.required = true;
+
+      var inputEstado = document.createElement("input");
+      inputEstado.type = "text";
+      inputEstado.name = "estado";
+      inputEstado.placeholder = "Estado";
+      inputEstado.required = true;
+
+      var inputCod_Cl = document.createElement("input");
+      inputCod_Cl.type = "text";
+      inputCod_Cl.name = "cod_Cl";
+      inputCod_Cl.placeholder = "Codigo del cliente";
+      inputCod_Cl.required = true;
+
+      var inputIdServicio = document.createElement("input");
+      inputIdServicio.type = "text";
+      inputIdServicio.name = "idServicio";
+      inputIdServicio.placeholder = "IdServicio";
+      inputIdServicio.required = true;
+
+      var submitButton = document.createElement("button");
+      submitButton.type = "submit";
+      submitButton.textContent = "Añadir Factura";
+
+      
+      form.appendChild(inputIdFactura);
+      form.appendChild(inputFecha);
+      form.appendChild(inputEstado);
+      form.appendChild(inputCod_Cl);
+      form.appendChild(inputIdServicio);
+      form.appendChild(submitButton);
+
+      
+      var btnAddFacturas = document.getElementById("btnAddFacturas");
+      btnAddFacturas.parentNode.replaceChild(form, btnAddFacturas);
+    });
+  </script>
 </body>
 </html>
--- paginasweb/vehiculos.php
+++ paginasweb/vehiculos.php
@@ -16,81 +16,241 @@
 </head>
 <body>
   <header class="menuheader">
-  <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" style="display: inline;">
-  <input type="hidden" name="logout">
-  <button type="submit" class="button-primary">Cerrar Sesión</button>
-</form>     
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" style="display: inline;">
+      <input type="hidden" name="logout">
+      <button type="submit" class="button-primary">Cerrar Sesión</button>
+    </form>
   </header>
+
   <div class="lateral">
     <div class="btn-group-vertical">
-    <a href="clientes.php" class="btn btn-drak btn-block" role="button">Clientes</a>
-  <a href="facturas.php" class="btn btn-drak btn-block" role="button">Facturas</a>
-  <a href="servicios.php" class="btn btn-drak btn-block" role="button">Servicios</a>
-  <a href="empleados.php" class="btn btn-drak btn-block" role="button">Empleados</a>
-</div>
-</div>
+      <a href="empleados.php" class="btn btn-drak btn-block" role="button">Empleados</a>
+      <a href="facturas.php" class="btn btn-drak btn-block" role="button">Facturas</a>
+      <a href="servicios.php" class="btn btn-drak btn-block" role="button">Servicios</a>
+      <a href="clientes.php" class="btn btn-drak btn-block" role="button">Clientes</a>
+    </div>
+  </div>
+
   <section class="content">
     <aside class="category">
     </aside>
     <div class="rectangulo">
-    <a href="#" class="button-secondary">Añadir Vehiculo</a>
-</div>
+      <a href="#" class="button-secondary" id="btnAddVehiculo">Añadir Vehiculo</a>
+    </div>
     <div class="destaque-prod">
-    <?php 
-$username = "root"; 
-$password = "ASf1*7JOSEM"; 
-$database = "taller_augusta"; 
-$mysqli = new mysqli("localhost", $username, $password, $database);
-$query = "SELECT * FROM vehiculo";
+      <?php 
+      $username = "root"; 
+      $password = "ASf1*7JOSEM"; 
+      $database = "taller_augusta"; 
+      $mysqli = new mysqli("localhost", $username, $password, $database);
+      $query = "SELECT * FROM vehiculo";
 
+      echo '<table border="1" cellspacing="1" cellpadding="1" style="max-width: 500px;"> 
+            <tr> 
+                <td> <font face="Arial">Matricula</font> </td> 
+                <td> <font face="Arial">Modelo</font> </td> 
+                <td> <font face="Arial">Orden</font> </td> 
+                <td> <font face="Arial">Marca</font> </td> 
+                <td> <font face="Arial">Color</font> </td>
+                <td> <font face="Arial">Cod_Em</font> </td> 
+                <td> <font face="Arial">Cod_Cl</font> </td> 
+                <td> <font face="Arial">Tipo_Vehiculo</font> </td> 
+            </tr>';
 
-echo '<table border="1" cellspacing="1" cellpadding="1"> 
-      <tr> 
-          <td> <font face="Arial">Modelo</font> </td> 
-          <td> <font face="Arial">Orden</font> </td> 
-          <td> <font face="Arial">Marca</font> </td> 
-          <td> <font face="Arial">Color</font> </td> 
-          <td> <font face="Arial">Cod_Em</font> </td>
-          <td> <font face="Arial">Cod_Cl</font> </td> 
-          <td> <font face="Arial">Tipo_Vehiculo</font> </td> 
-      </tr>';
+      if ($result = $mysqli->query($query)) {
+          while ($row = $result->fetch_assoc()) {
+              $field1name = $row["Matricula"];
+              $field2name = $row["Modelo"];
+              $field3name = $row["Orden"];
+              $field4name = $row["Marca"];
+              $field5name = $row["Color"];
+              $field6name = $row["Cod_Em"];
+              $field7name = $row["Cod_Cl"];
+              $field8name = $row["Tipo_Vehiculo"]; 
 
-if ($result = $mysqli->query($query)) {
-    while ($row = $result->fetch_assoc()) {
-        $field1name = $row["Modelo"];
-        $field2name = $row["Orden"];
-        $field3name = $row["Marca"];
-        $field4name = $row["Color"];
-        $field5name = $row["Cod_Em"];
-        $field6name = $row["Cod_Cl"]; 
-        $field7name = $row["Tipo_Vehiculo"];  
+              echo '<tr> 
+                        <td>'.$field1name.'</td> 
+                        <td>'.$field2name.'</td> 
+                        <td>'.$field3name.'</td> 
+                        <td>'.$field4name.'</td> 
+                        <td>'.$field5name.'</td>
+                        <td>'.$field6name.'</td> 
+                        <td>'.$field7name.'</td> 
+                        <td>'.$field8name.'</td> 
+                    </tr>';
+          }
+          $result->free();
+      } 
+      ?>
 
-        echo '<tr> 
-                  <td>'.$field1name.'</td> 
-                  <td>'.$field2name.'</td> 
-                  <td>'.$field3name.'</td> 
-                  <td>'.$field4name.'</td> 
-                  <td>'.$field5name.'</td>
-                  <td>'.$field6name.'</td>
-                  <td>'.$field7name.'</td>
-              </tr>';
-    }
-    $result->free();
-} 
-?>
-<?php
-session_start();
+      <?php
+      session_start();
 
-if (isset($_POST['logout'])) {
-  session_unset();
-  header("Location: paginaprincipal.php");
-  exit();
-}
-?>
-    </div>    
+      if (isset($_POST['logout'])) {
+        session_unset();
+        header("Location: paginaprincipal.php");
+        exit();
+      }
+
+      if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["Matricula"]) && isset($_POST["Modelo"]) && isset($_POST["Orden"]) && isset($_POST["Marca"]) && isset($_POST["Color"])&& isset($_POST["Cod_Em"]) && isset($_POST["Cod_Cl"]) && isset($_POST["Tipo_Vehiculo"])) {
+        $Matricula = $_POST["Matricula"];
+        $Modelo = $_POST["Modelo"];
+        $Orden = $_POST["Orden"];
+        $Marca = $_POST["Marca"];
+        $Color = $_POST["Color"];
+        $Cod_Em = $_POST["Cod_Em"];
+        $Cod_Cl = $_POST["Cod_Cl"];
+        $Tipo_Vehiculo = $_POST["Tipo_Vehiculo"];
+
+        $username = "root"; 
+        $password = "ASf1*7JOSEM"; 
+        $database = "taller_augusta"; 
+        $mysqli = new mysqli("localhost", $username, $password, $database);
+
+        
+        $checkQuery = "SELECT Matricula FROM vehiculo WHERE Matricula = '$Matricula'";
+        $checkResult = $mysqli->query($checkQuery);
+        if ($checkResult->num_rows > 0) {
+          echo "La matricula ya existe. Por favor, elija otro código.";
+        } else {
+          $query = "INSERT INTO vehiculo (Matricula, Modelo, Orden, Marca, Color, Cod_Em, Cod_Cl, Tipo_Vehiculo) VALUES ('$Matricula', '$Modelo', '$Orden', '$Marca', '$Color', '$Cod_Em', '$Cod_Cl', '$Tipo_Vehiculo')";
+
+          if ($mysqli->query($query)) {
+            echo "El nuevo vehiculo se ha añadido correctamente a la base de datos.";
+          } else {
+            echo "Error al añadir el nuevo vehiculo: " . $mysqli->error;
+          }
+        }
+
+        $mysqli->close();
+
+        
+        header("Location: ".$_SERVER['PHP_SELF']);
+      }
+
+      session_start();
+
+      if (isset($_POST['logout'])) {
+        session_unset();
+        header("Location: paginaprincipal.php");
+        exit();
+      }
+
+      if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["codVe_eliminar"])) {
+        $codVe_eliminar = $_POST["codVe_eliminar"];
+
+        $username = "root"; 
+        $password = "ASf1*7JOSEM"; 
+        $database = "taller_augusta"; 
+        $mysqli = new mysqli("localhost", $username, $password, $database);
+
+        
+        $checkQuery = "SELECT Matricula FROM vehiculo WHERE Matricula = '$codVe_eliminar'";
+        $checkResult = $mysqli->query($checkQuery);
+        if ($checkResult->num_rows > 0) {
+          $deleteQuery = "DELETE FROM vehiculo WHERE Matricula = '$codVe_eliminar'";
+          if ($mysqli->query($deleteQuery)) {
+            echo "El vehiculo se ha eliminado correctamente de la base de datos.";
+          } else {
+            echo "Error al eliminar el vehiculo: " . $mysqli->error;
+          }
+        } else {
+          echo "La matricula no existe en la base de datos. Por favor, ingrese un código válido.";
+        }
+
+        $mysqli->close();
+
+        
+        header("Location: ".$_SERVER['PHP_SELF']);
+      }
+      ?>
+      <hr>
+      <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <input type="text" name="codVe_eliminar" placeholder="La matricula a Eliminar" required>
+        <button type="submit" class="button-primary">Eliminar Vehiculo</button>
+      </form>
+    </div>
   </section>
+
   <footer class="footer">
-    
-  </footer>
+  </footer>    
+
+  <script>
+    document.getElementById("btnAddVehiculo").addEventListener("click", function(event) {
+      event.preventDefault(); 
+
+     
+      var form = document.createElement("form");
+      form.method = "post";
+      form.action = "<?php echo $_SERVER['PHP_SELF']; ?>";
+
+      var inputMatricula = document.createElement("input");
+      inputMatricula.type = "text";
+      inputMatricula.name = "Matricula";
+      inputMatricula.placeholder = "Matricula";
+      inputMatricula.required = true;
+
+      var inputModelo = document.createElement("input");
+      inputModelo.type = "text";
+      inputModelo.name = "Modelo";
+      inputModelo.placeholder = "Modelo";
+      inputModelo.required = true;
+
+      var inputOrden = document.createElement("input");
+      inputOrden.type = "text";
+      inputOrden.name = "Orden";
+      inputOrden.placeholder = "Orden";
+      inputOrden.required = true;
+
+      var inputMarca = document.createElement("input");
+      inputMarca.type = "text";
+      inputMarca.name = "Marca";
+      inputMarca.placeholder = "Marca";
+      inputMarca.required = true;
+
+      var inputColor = document.createElement("input");
+      inputColor.type = "text";
+      inputColor.name = "Color";
+      inputColor.placeholder = "Color";
+      inputColor.required = true;
+
+      var inputCod_Em = document.createElement("input");
+      inputCod_Em.type = "text";
+      inputCod_Em.name = "Cod_Em";
+      inputCod_Em.placeholder = "Cod_Em";
+      inputCod_Em.required = true;
+
+      var inputCod_Cl = document.createElement("input");
+      inputCod_Cl.type = "text";
+      inputCod_Cl.name = "Cod_Cl";
+      inputCod_Cl.placeholder = "Cod_Cl";
+      inputCod_Cl.required = true;
+
+      var inputTipo_Vehiculo = document.createElement("input");
+      inputTipo_Vehiculo.type = "text";
+      inputTipo_Vehiculo.name = "Tipo_Vehiculo";
+      inputTipo_Vehiculo.placeholder = "Tipo_Vehiculo";
+      inputTipo_Vehiculo.required = true;
+
+      var submitButton = document.createElement("button");
+      submitButton.type = "submit";
+      submitButton.textContent = "Añadir Vehiculo";
+
+      
+      form.appendChild(inputMatricula);
+      form.appendChild(inputModelo);
+      form.appendChild(inputOrden);
+      form.appendChild(inputMarca);
+      form.appendChild(inputColor);
+      form.appendChild(inputCod_Em);
+      form.appendChild(inputCod_Cl);
+      form.appendChild(inputTipo_Vehiculo);
+      form.appendChild(submitButton);
+
+      
+      var btnAddVehiculo = document.getElementById("btnAddVehiculo");
+      btnAddVehiculo.parentNode.replaceChild(form, btnAddVehiculo);
+    });
+  </script>
 </body>
 </html>
